4cd3e8ba210b64ae53e8a1c76aa4eae5a861ef0a
EC2 Default User <ec2-user@ip-172-31-16-97.us-east-2.compute.internal>
2018-04-26 00:55:03 +0000

    styling

--- app/gradient_example_store/routes.py
+++ app/gradient_example_store/routes.py
@@ -2,14 +2,28 @@ import os
 import sys
 import random
 import subprocess
+import requests
 from datetime import datetime
-from flask import Blueprint, render_template
+
+from flask import Blueprint, render_template, request, jsonify
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('checkout.html')
+    return render_template('index.html')
+    
+
+@bp.route('/checkout')
+def checkout():
+    url = request.args.get('url')
+    vendor_id = request.args.get('vendor_id')
+    request_url = url + "/checkout/vendor_name?vendor_id=" + vendor_id
+    vendor_name = requests.get(request_url).content.decode('utf-8')
+    return render_template('checkout.html', 
+        url = url, 
+        vendor_id = vendor_id, 
+        vendor_name = vendor_name)
 
 
 @bp.route('/complete_checkout')
